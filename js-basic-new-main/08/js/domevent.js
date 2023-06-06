134a76886afe5812a05c399692d20f2bcd73fe7b
big-choi <bos33210@naver.com>
2023-06-06 14:47:11 +0900

    이벤트 처리 방법

--- js-basic-new-main/08/js/domevent.js
+++ js-basic-new-main/08/js/domevent.js
@@ -1,6 +1,15 @@
     var pic = document.querySelector('#pic');
-    
-		function changePic() {			
+
+    pic.addEventListener("mouseover", changePic, false);
+    pic.addEventListener("mouseout", originPic, false);
+
+    document.addEventListener('click', sayHello, false);
+
+    function sayHello () {
+      alert("안녕하세요");
+    }
+
+		function changePic() {
 			pic.src = "images/boy.png";
     }
     function originPic() {
