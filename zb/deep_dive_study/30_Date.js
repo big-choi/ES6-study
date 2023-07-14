9f79571de91695dcfbc0eeec219aa89454193c07
big-choi <bos33210@naver.com>
2023-07-14 14:58:49 +0900

    30장 Date

---
+++ zb/deep_dive_study/30_Date.js
@@ -0,0 +1,52 @@
+function generateLottoNumbers() {
+  let lottoNumbers = new Set();
+
+  while (lottoNumbers.size < 6) {
+    let randomNumber = Math.floor(Math.random() * 45) + 1;
+    lottoNumbers.add(randomNumber);
+  }
+
+  return Array.from(lottoNumbers);
+}
+
+const koreanLottoNumbers = generateLottoNumbers();
+console.log(koreanLottoNumbers);
+
+let words = ["spray", "limit", "elite", "exuberant", "destruction", "present"];
+const appendedWords = words.filter((word, index, arr) => {
+  arr.push("new");
+  return word.length < 6;
+});
+
+// 30.3 Date를 활용한 시간 예제
+
+(function printNow() {
+  const today = new Date();
+
+  const dayNames = ['(일요일)', '(월요일)', '(화요일)', '(수요일)', '(목요일)', '(금요일)', '(토요일)'];
+
+  // getDay: 요일을 나타내는 정수를 반환한다. (0 ~ 6)
+  const day = dayNames[today.getDay()];
+
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
+  const ampm = hour >= 12 ? 'PM' : 'AM';
+
+  // 12시간제로 변경
+  hour %= 12;
+  hour = hour || 12; // 0 => 12
+
+  // 10미만인 분과 초를 2자리로 변경
+  minute = minute < 10 ? '0' + minute : minute;
+  second = second < 10 ? '0' + second : second;
+
+  const now = `${year}년 ${month}월 ${date}일 ${day} ${hour}:${minute}:${second} ${ampm}`;
+
+  console.log(now);
+
+  setTimeout(printNow, 1000);
+})();
